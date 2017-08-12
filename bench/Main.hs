7e77d1b3276f8aa6566134e5e9dc4506f823d6c9
John Ky <newhoggy@gmail.com>
2017-08-12 10:17:11 +1000

    More benchmarking

--- bench/Main.hs
+++ bench/Main.hs
@@ -12,7 +12,23 @@ setupEnvVector n = return $ DVS.fromList (take n (cycle [maxBound, 0]))
 
 benchRankSelect :: [Benchmark]
 benchRankSelect =
-  [ env (setupEnvVector 1000000) $ \bv -> bgroup "Rank"
+  [ env (return ()) $ \_ -> bgroup "8-bit"
+    [ bench "Rank - Once"   (whnf (rank1    (0x55 :: Word8)) 8)
+    , bench "Select - Once" (whnf (select1  (0x55 :: Word8)) 8)
+    ]
+  , env (return ()) $ \_ -> bgroup "16-bit"
+    [ bench "Rank - Once"   (whnf (rank1    (0x5555 :: Word16)) 16)
+    , bench "Select - Once" (whnf (select1  (0x5555 :: Word16)) 16)
+    ]
+  , env (return ()) $ \_ -> bgroup "32-bit"
+    [ bench "Rank - Once"   (whnf (rank1    (0x55555555 :: Word32)) 32)
+    , bench "Select - Once" (whnf (select1  (0x55555555 :: Word32)) 32)
+    ]
+  , env (return ()) $ \_ -> bgroup "64-bit"
+    [ bench "Rank - Once"   (whnf (rank1    (0x5555555555555555 :: Word64)) 64)
+    , bench "Select - Once" (whnf (select1  (0x5555555555555555 :: Word64)) 64)
+    ]
+  , env (setupEnvVector 1000000) $ \bv -> bgroup "64-bit vector"
     [ bench "Rank - Once"   (whnf (rank1    bv) 1)
     , bench "Select - Once" (whnf (select1  bv) 1)
     , bench "Rank - Many"   (nf   (map (rank1 bv)) [0, 1000..10000000])
